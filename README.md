dc8c0eb64b9048d39de8abddc97820f044139bd7
chawlaaditya8 <chawlaaditya8@gmail.com>
2015-12-02 12:46:12 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # node-login-system
+
+
+A basic node login system to kickstart node application.
+
+Build with the help of [braitsch/node-login](https://github.com/braitsch/node-login).
---
+++ app.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var cookieParser = require('cookie-parser');
+var MongoStore = require('connect-mongo')(session);
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/app/server/views');
+app.set('view engine', 'jade');
+app.use(cookieParser());
+app.use(session({
+	secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
+	proxy: true,
+	resave: true,
+	saveUninitialized: true,
+	store: new MongoStore({ host: 'localhost', port: 27017, db: 'node-login'})
+	})
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
+app.use(express.static(__dirname + '/app/public'));
+
+require('./app/server/routes')(app);
+
+if (app.get('env') == 'development') app.use(errorHandler());
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
